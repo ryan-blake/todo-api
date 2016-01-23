9578b0223c3b48e2bbf3c4f9a26d57091b09a343
Ryan Kliemann <Ryan_blake@icloud.com>
2016-01-22 16:56:20 -0800

    4-

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :lists
-  resources :users
+
+
   get 'home/index'
   root 'home#index'
 
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resources :users do
+        resources :lists
+      end
+
+      resources :lists, only: [] do
+        resources :items, only: [:create]
+      end
+
+      resources :items, only: [:destroy]
   end
 end
